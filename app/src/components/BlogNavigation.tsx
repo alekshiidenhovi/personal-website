618551d9153a3b2743aa04250c213cdb858ba9d6
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-09-29 23:21:48 +0300

    Align aside with blog text

--- app/src/components/BlogNavigation.tsx
+++ app/src/components/BlogNavigation.tsx
@@ -9,35 +9,46 @@ interface BlogNavigationProps {
 }
 
 export default function BlogNavigation({ sectionLinks }: BlogNavigationProps) {
-  const [activeSlug, setActiveSlug] = useState<string>(
-    sectionLinks[0]?.slug || "",
-  );
+  // const [activeSlug, setActiveSlug] = useState<string>(
+  //   sectionLinks[0]?.slug || "",
+  // );
 
-  const handleIntersection = (
-    inView: boolean,
-    entry: IntersectionObserverEntry,
-  ) => {
-    const slug = entry.target.getAttribute("href") || "";
-    console.log(`Slug ${slug} is in view: ${inView}`);
-    if (inView) {
-      setActiveSlug(slug);
-    }
-  };
+  // const handleIntersection = (
+  //   inView: boolean,
+  //   entry: IntersectionObserverEntry,
+  // ) => {
+  //   const slug = entry.target.getAttribute("href") || "";
+  //   console.log(`Slug ${slug} is in view: ${inView}`);
+  //   if (inView) {
+  //     setActiveSlug(slug);
+  //   }
+  // };
 
-  // Apply `useInView` hook to observe each section
-  const pixelOffset = 32;
-  const windowHeight = typeof window !== "undefined" ? window.innerHeight : 0;
-  const triggerHeight = 16;
-  const topOffset = `${pixelOffset}px`;
-  const bottomOffset = `${windowHeight - pixelOffset + triggerHeight}px`;
-  const { ref } = useInView({
-    rootMargin: `${topOffset} 0px ${bottomOffset} 0px`, // Trigger the callback when 50% of the section is visible
-    threshold: 0,
-    onChange: handleIntersection, // Trigger the intersection callback
-  });
+  // // Apply `useInView` hook to observe each section
+  // const pixelOffset = 32;
+  // const windowHeight = typeof window !== "undefined" ? window.innerHeight : 0;
+  // const triggerHeight = 16;
+  // const topOffset = `${pixelOffset}px`;
+  // const bottomOffset = `${windowHeight - pixelOffset + triggerHeight}px`;
+  // const { ref } = useInView({
+  //   rootMargin: `${topOffset} 0px ${bottomOffset} 0px`, // Trigger the callback when 50% of the section is visible
+  //   threshold: 0,
+  //   onChange: handleIntersection, // Trigger the intersection callback
+  // });
+
+  // const navigationId = "blog-navigation";
+  // const introHeading = document.getElementById("introduction");
+  // const wrapper = document.getElementById("page-wrapper");
+  // const wrapperRect = wrapper?.getBoundingClientRect();
+  // const initialTopOffset = headingRect.top - wrapperRect.top;
+  // const topOffset = `${initialTopOffset / 16}rem`;
+  const topOffset = "4rem";
 
   return (
-    <aside className="sticky top-16 h-fit max-w-64">
+    <aside
+      // id={navigationId}
+      className={`sticky top-16 mt-[18.8rem] h-fit max-w-64`}
+    >
       <span className="flex pb-6 text-3xl font-semibold text-neutral-200 text-opacity-70">
         Table of Contents
       </span>
@@ -46,10 +57,9 @@ export default function BlogNavigation({ sectionLinks }: BlogNavigationProps) {
           <BlogSectionLink
             key={heading.slug}
             href={heading.slug}
-            isActive={heading.slug === activeSlug}
+            isActive={false}
             text={heading.text}
             depth={heading.depth as 2 | 3 | 4 | 5 | 6}
-            ref={ref}
           />
         ))}
       </nav>
