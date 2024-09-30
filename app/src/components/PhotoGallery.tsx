f1ca7b537b722008425b1f3e458f8758c235da47
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-09-30 10:48:22 +0300

    Intermediate photogallery

---
+++ app/src/components/PhotoGallery.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { MDXProvider } from "@mdx-js/react";
+
+interface Image {
+  src: string;
+  alt: string;
+}
+
+const PhotoGallery = ({
+  images,
+  caption,
+}: {
+  images: Image[];
+  caption: string;
+}) => {
+  return (
+    <MDXProvider>
+      <figure className="flex flex-col items-center gap-1">
+        <div className="flex gap-4">
+          {images.map((image) => (
+            <picture className="h-full w-full scale-90 overflow-hidden rounded-2xl border-2 border-transparent transition-all duration-300 hover:scale-100 hover:border-primary-500">
+              <img
+                className="h-full w-full object-cover"
+                src={image.src}
+                alt={image.alt}
+              />
+            </picture>
+          ))}
+        </div>
+        <figcaption className="prose w-full text-center text-lg text-neutral-500">
+          {caption}
+        </figcaption>
+      </figure>
+    </MDXProvider>
+  );
+};
+
+export default PhotoGallery;
