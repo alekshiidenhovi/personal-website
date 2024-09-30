be806bc1a0103e8d6f7aae5a3c8f0fe6b1d1b25c
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2024-09-30 10:52:06 +0300

    add images

--- app/src/components/PhotoGallery.tsx
+++ app/src/components/PhotoGallery.tsx
@@ -19,7 +19,8 @@ const PhotoGallery = ({
       <figure className="flex flex-col items-center gap-1">
         <div className="flex gap-4">
           {images.map((image) => (
-            <picture className="h-full w-full scale-90 overflow-hidden rounded-2xl border-2 border-transparent transition-all duration-300 hover:scale-100 hover:border-primary-500">
+            <picture className="group relative h-full w-full scale-90 overflow-hidden rounded-2xl border-2 border-transparent transition-all duration-300 hover:scale-100 hover:border-primary-500">
+              <div className="absolute inset-0 bg-black opacity-20 transition-opacity duration-300 group-hover:opacity-0" />
               <img
                 className="h-full w-full object-cover"
                 src={image.src}
