83f5c543faa55a8a87e0e7db0737af2022618f7e
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2026-01-07 18:40:11 +0200

    add descriptive docstring to root app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
-<script setup lang="ts">
+<script lang="ts">
 import HeroSection from "./components/HeroSection.vue";
 import ExpertiseSection from "./components/ExpertiseSection.vue";
+
+/**
+ * Root component for the personal website
+ */
+export default {
+  name: "App",
+  components: {
+    HeroSection,
+    ExpertiseSection
+  },
+  setup() { }
+}
 </script>
 
 <template>
