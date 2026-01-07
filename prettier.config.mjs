25d54fa54163a77050323c297f44365bb882b0f2
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2026-01-07 20:43:36 +0200

    remove tailwind and its dependencies

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,17 +1,12 @@
-/** @type {import("@trivago/prettier-plugin-sort-imports").PrettierConfig & import("prettier-plugin-tailwindcss").PluginOptions} */
+/** @type {import("@trivago/prettier-plugin-sort-imports").PrettierConfig} */
 const prettierConfig = {
-  plugins: [
-    "@trivago/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
-  ],
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
   trailingComma: "all",
   singleQuote: false,
   semi: true,
   importOrder: ["^[A-Za-z]/(.*)$", "^@ui/(.*)$", "^@(.*)$", "^[./]"],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
-  tailwindConfig: "tailwind.config.ts",
-  tailwindFunctions: ["tv"], // Read more: https://github.com/tailwindlabs/prettier-plugin-tailwindcss#sorting-classes-in-function-calls
 };
 
 export default prettierConfig;
