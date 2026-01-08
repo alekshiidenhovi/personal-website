7f272b0f436411307795c9f70394f6a73552ce1a
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2026-01-08 20:29:39 +0200

    setup cloudflare workers plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [vue()],
+  plugins: [vue(), cloudflare()],
 });
